d815709855b08a09133c75a270ef1135f64d1d2e
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2025-12-31 20:50:11 +0900

    feature_pitcher_basic_logic

---
+++ Assets/Scripts/Pitcher/PitcherCtrl.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitcherCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private Ball ballScript;
+    [SerializeField]
+    private Transform ballSpawnPoint;
+    [SerializeField]
+    private Camera pitcherCam;
+    [SerializeField]
+    private GameObject targetObj;
+
+    private bool is_throw = true;
+
+    public float PitchSpeed = 10f;
+    public Vector3 TargetPosition;
+    public bool hasTarget = false;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Pitch();
+        Debug.Log("PitcherCtrl_ Update");
+        if (Input.GetMouseButtonDown(0))
+        {
+            is_throw = false;
+            ballScript.gameObject.SetActive(true);
+            ballScript.ThrowBall();
+        }
+
+    }
+
+    void Pitch()
+    {
+        if (is_throw)
+        {
+            Ray ray = pitcherCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, LayerMask.GetMask("PitchArea")))
+            {
+                TargetPosition = hit.point;
+                hasTarget = true;
+                targetObj.transform.position = TargetPosition;
+            }
+        }        
+    }
+    public void BallToTarget()
+    {
+        ballScript.gameObject.SetActive(false);
+        is_throw = true;
+    }
+}
