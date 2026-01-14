4ed27b260239251be52409d17651abec6978301a
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2026-01-14 20:58:43 +0900

    feature-pitching_logic
    - 정확도 로직을 제외한 투구 로직 구현

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public enum PitchType
+{
+    FastBall,//직구
+    CurveBall,//커브
+    SliderBall//슬라이더
+}
+public enum CameraMod
+{
+    Pitcher_Cam, //투수 카메라
+    Hitter_Cam, //타자 카메라
+    MainCam //메인 카메라
+}
+
+public enum PitchState
+{
+    Idle,//아무것도 안하는 상태(기본 상태)
+    SelectingPitchType,//구종 선택
+    SelectingTarget,// 타겟위치 선택
+    SetAccuracy,//정확도 조정
+    Throwing//던지는 상태(최종 상태)
+}
+
+
