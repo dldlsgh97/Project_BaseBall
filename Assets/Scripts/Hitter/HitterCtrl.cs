7bf2a7c9af0c4d03ade87d2c3729cd58dc2e0c5a
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2026-02-09 21:46:36 +0900

    feature_HitterAnimation
    - 타자스윙 애니메이션 추가

--- Assets/Scripts/Hitter/HitterCtrl.cs
+++ Assets/Scripts/Hitter/HitterCtrl.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class HitterCtrl : MonoBehaviour
 {
-    void Start()
+    [SerializeField]
+    private Animator anim;
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
-    void Update()
+    public void OnHit()
     {
-        
+        Debug.Log("OnHit");
+        //타격판정 로직구현(직접 구현X 판단 주체 스크립트에서 판단)
     }
+
+    public void OnClickSwingBtn()
+    {
+        anim.Play("HitterSwing", 0, 0f);
+    }
+
 }
